74e31c20cfca2bf8026a6be129eb5a0941566f39
wyvri <wrennuin@gmail.com>
2024-04-03 11:11:35 -0500

    more!!

--- djangotutorials/djangotutorials/settings.py
+++ djangotutorials/djangotutorials/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutorialapp',
+    'tutortialapp',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangotutorials.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
