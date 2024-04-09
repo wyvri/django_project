8eb9dd6e377d82633ea02950b7d55f7301fb289f
wyvri <wrennuin@gmail.com>
2024-04-09 11:43:23 -0500

    add a model to the database

--- djangotutorials/tutortialapp/models.py
+++ djangotutorials/tutortialapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+
+    GRADES = (
+        ('9', '9'),
+        ('10', '10'),
+        ('11', '11'),
+        ('12', '12'),
+    )
+
+    firstname = models.CharField(max_length=200,null=True)
+    lastname = models.CharField(max_length=200,null=True)
+    middlename = models.CharField(max_length=200,null=True)
+    grade = models.CharField(max_length=200,null=True, choices=GRADES)
+
+    def __str__(self) -> str:
+        return f"{self.lastname}, {self.firstname}, {self.middlename}, {self.grade}"
