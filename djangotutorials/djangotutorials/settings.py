fa8987de6d43b3dbe38fbb7025381be5c690322c
wyvri <wrennuin@gmail.com>
2024-04-25 12:13:23 -0500

    man

--- djangotutorials/djangotutorials/settings.py
+++ djangotutorials/djangotutorials/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/djangotutorials/tutorialapp/static'
 STATIC_URL = '/static/'
