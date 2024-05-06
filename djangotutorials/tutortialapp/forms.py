32d4c12df846c6fdc51f3d4dd7c880ac1f93121e
wyvri <wrennuin@gmail.com>
2024-05-06 11:36:32 -0500

    create profile form

--- djangotutorials/tutortialapp/forms.py
+++ djangotutorials/tutortialapp/forms.py
@@ -18,3 +18,8 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['firstname', 'lastname']
